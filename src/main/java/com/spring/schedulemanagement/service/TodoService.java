e1ab00fd0767e7e6f0c1853dabf4024299bafe80
joacho <joacho1205@gmail.com>
2024-12-10 01:26:45 +0900

    3 layer 역할 분리

--- src/main/java/com/spring/schedulemanagement/service/TodoService.java
+++ src/main/java/com/spring/schedulemanagement/service/TodoService.java
@@ -1,4 +1,26 @@
 package com.spring.schedulemanagement.service;
 
+import com.spring.schedulemanagement.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
+    // 속성
+    private final TodoRepository todoRepository;
+
+    // 생성자
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    // 기능
+    // ::: 일정 생성 서비스
+    public String createTodoService(String data) {
+        // 비즈니스로직
+        System.out.println(data);
+        String result = todoRepository.createTodo(data);
+        return result;
+    }
+
+    // 일정 조회 서비스 메서드 만들기
 }
